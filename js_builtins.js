e4a05c2ca0e23cc33c580d3e21f37930023ef4b7
Duo Xu <xxxuduo@berkeley.edu>
2019-02-21 14:17:36 -0800

    finish hw01

--- js_builtins.js
+++ js_builtins.js
@@ -22,7 +22,7 @@ window.builtins = {};
 builtins.trim = function(str) {
   let newStr = "";
   for (let i = 0; i < str.length; i++) {
-    if (str[i] !== " ") {
+    if (str[i] !== " " || str[i - 1] !== " " && str[i + 1] !== " ") {
       newStr += str[i];
     }
   }
@@ -45,11 +45,7 @@ builtins.trim = function(str) {
 // ex. builtins.search('Horizons', 'h') -> false
 
 builtins.search = function(sourceString, searchString) {
-  if (searchString.indexOf(sourceString)) {
-    return true;
-  } else {
-    return false;
-  }
+  return sourceString.match(searchString) !== null;
 };
 
 // ----------------------------------------------------------------------------
@@ -65,6 +61,11 @@ builtins.search = function(sourceString, searchString) {
 // ex. builtins.reverse([123]) -> [123]
 
 builtins.reverse = function(arr) {
-  let next = arr.unshift();
-  return builtins.reverse(arr).concat(next);
+  var newarr  = new Array();
+  var j = 0
+  for(let i = arr.length - 1; i >= 0; i--)  {
+    newarr[j] = arr[i];
+    j++;
+  }
+  return newarr;
 };
